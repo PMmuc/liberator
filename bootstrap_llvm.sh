0fe468b7ceebd2be157c0414d2aba32417fcbb40
PMmuc <pm@mm-muc.de>
2025-11-05 00:27:46 +0100

    format

--- bootstrap_llvm.sh
+++ bootstrap_llvm.sh
@@ -9,15 +9,15 @@ echo "[INFO] Install clang-12"
 sudo ./update-alternatives-clang.sh 12 200
 
 if [ ! -d llvm-project ]; then
-    echo "[INFO] llvm-project not present, download it"
-    # download LLVM 14
-    ./install_llvm.sh
+  echo "[INFO] llvm-project not present, download it"
+  # download LLVM 14
+  ./install_llvm.sh
 else
-    echo "[INFO] llvm-project already here! I assume it is OK"
+  echo "[INFO] llvm-project already here! I assume it is OK"
 fi
 
 cp custom-libfuzzer/build.sh llvm-project/
 
-# compile my LLVM 14
+# compile my LLVM 16
 echo "[INFO] compile LLVM"
-./llvm-project/build.sh 
+./llvm-project/build.sh
