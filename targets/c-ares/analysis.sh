01bd283be852b3a9cd3ea9d087231d1ea231bf29
PMmuc <pm@mm-muc.de>
2026-03-01 17:31:50 +0100

    update for gprof

--- targets/c-ares/analysis.sh
+++ targets/c-ares/analysis.sh
@@ -5,7 +5,7 @@ set -x
 
 # NOTE: if TOOLD_DIR is unset, I assume to find stuffs in LIBFUZZ folder
 if [ -z "$TOOLS_DIR" ]; then
-    TOOLS_DIR=$LIBFUZZ
+  TOOLS_DIR=$LIBFUZZ
 fi
 
 WORK="$TARGET/work"
@@ -24,18 +24,16 @@ export LLVM_COMPILER_PATH=$LLVM_DIR/bin
 export LIBFUZZ_LOG_PATH=$WORK/apipass
 # export CFLAGS="-mllvm -get-api-pass"
 
-
 mkdir -p "$LIBFUZZ_LOG_PATH"
 
 echo "make 1"
 mkdir -p "$TARGET/repo/cares_build"
 cd "$TARGET/repo/cares_build"
 
-
 cmake .. -DCMAKE_INSTALL_PREFIX="$WORK" -DBUILD_SHARED_LIBS=off \
-        -DENABLE_STATIC=on -DCMAKE_BUILD_TYPE=Debug -DCARES_STATIC=on \
-        -DCMAKE_C_FLAGS_DEBUG="-g -O0" \
-        -DCMAKE_CXX_FLAGS_DEBUG="-g -O0"
+  -DENABLE_STATIC=on -DCMAKE_BUILD_TYPE=Debug -DCARES_STATIC=on \
+  -DCMAKE_C_FLAGS_DEBUG="-g -O0" \
+  -DCMAKE_CXX_FLAGS_DEBUG="-g -O0"
 # configure compiles some shits for testing, better remove it
 rm -rf "$LIBFUZZ_LOG_PATH"/apis.log
 
@@ -57,18 +55,19 @@ extract-bc -b "$WORK"/lib/libcares_static.a
 # this extracts the exported functions in a file, to be used later for grammar
 # generations
 "$TOOLS_DIR"/tool/misc/extract_included_functions.py -i "$WORK/include" \
-    -p "$LIBFUZZ/targets/${TARGET_NAME}/public_headers.txt" \
-    -e "$LIBFUZZ_LOG_PATH/exported_functions.txt" \
-    -t "$LIBFUZZ_LOG_PATH/incomplete_types.txt" \
-    -a "$LIBFUZZ_LOG_PATH/apis_clang.json" \
-    -n "$LIBFUZZ_LOG_PATH/enum_types.txt"
+  -p "$LIBFUZZ/targets/${TARGET_NAME}/public_headers.txt" \
+  -e "$LIBFUZZ_LOG_PATH/exported_functions.txt" \
+  -t "$LIBFUZZ_LOG_PATH/incomplete_types.txt" \
+  -a "$LIBFUZZ_LOG_PATH/apis_clang.json" \
+  -n "$LIBFUZZ_LOG_PATH/enum_types.txt"
 
+cd $WORK
 # extract fields dependency from the library itself, repeat for each object
 # produced
 "$TOOLS_DIR"/condition_extractor/bin/extractor \
-    "$WORK"/lib/libcares_static.a.bc \
-    -interface "$LIBFUZZ_LOG_PATH/apis_clang.json" \
-    -output "$LIBFUZZ_LOG_PATH/conditions.json" \
-    -minimize_api "$LIBFUZZ_LOG_PATH/apis_minimized.txt" \
-    -v v0 -t json -do_indirect_jumps \
-    -data_layout "$LIBFUZZ_LOG_PATH/data_layout.txt"
+  "$WORK"/lib/libcares_static.a.bc \
+  -interface "$LIBFUZZ_LOG_PATH/apis_clang.json" \
+  -output "$LIBFUZZ_LOG_PATH/conditions.json" \
+  -minimize_api "$LIBFUZZ_LOG_PATH/apis_minimized.txt" \
+  -v v0 -t json -do_indirect_jumps \
+  -data_layout "$LIBFUZZ_LOG_PATH/data_layout.txt"
