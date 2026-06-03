759f7f41404817f8dd34b65cd4c7d83a29696696
PMmuc <pm@mm-muc.de>
2026-06-03 16:10:31 +0200

    output unresolved function calls

--- docker/run_analysis.sh
+++ docker/run_analysis.sh
@@ -6,31 +6,31 @@
 ##
 
 if [ -z "$TARGET" ]; then
-    echo "[ERROR] \$TARGET must be specified as an environment variable"
-    exit 1
+  echo "[ERROR] \$TARGET must be specified as an environment variable"
+  exit 1
 fi
 
 IMG_NAME="libpp-analysis"
 LIBPP=../
 
 if [ -s "$LIBPP/analysis/$TARGET/work/apipass/conditions.json" ]; then
-    echo "[INFO] Analysis for $TARGET already exists"
-    exit 0
+  echo "[INFO] Analysis for $TARGET already exists"
+  exit 0
 fi
 
 set -x
 DOCKER_BUILDKIT=1 docker build \
-    --build-arg USER_UID=$(id -u) --build-arg GROUP_UID=$(id -g) \
-    -t "$IMG_NAME" --target libfuzzpp_analysis \
-    -f "$LIBPP/Dockerfile" "$LIBPP"
+  --build-arg USER_UID=$(id -u) --build-arg GROUP_UID=$(id -g) \
+  -t "$IMG_NAME" --target libfuzzpp_analysis \
+  -f "$LIBPP/Dockerfile" "$LIBPP"
 set +x
 
 echo "$IMG_NAME"
 
 if [[ "${DEVENV}" ]]; then
-    docker run --env TARGET=${TARGET} -v "$(pwd)/..:/workspaces/libfuzz" \
-        "$IMG_NAME"
+  docker run --env TARGET=${TARGET} -v "$(pwd)/..:/workspaces/libfuzz" \
+    "$IMG_NAME" /bin/zsh
 else
-    docker run --rm -d --name "${IMG_NAME}-${TARGET}" \
-        --env TARGET=${TARGET} -v "$(pwd)/..:/workspaces/libfuzz" "$IMG_NAME"
+  docker run --rm -it --name "${IMG_NAME}-${TARGET}" \
+    --env TARGET=${TARGET} -v "$(pwd)/..:/workspaces/libfuzz" "$IMG_NAME" /bin/zsh
 fi
